9bdd620444b13cbdc555c1bd534765cf2d427aa3
Ronaldo Silva <ronaldo.intermec@gmail.com>
2024-02-09 10:53:32 -0300

    change title from vivi

--- lib/presentation/pages/home.dart
+++ lib/presentation/pages/home.dart
@@ -13,7 +13,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Vivi'),
+        title: const Text('Cadastro de Falhas'),
       ),
       body: Stack(
         alignment: Alignment.topCenter,
