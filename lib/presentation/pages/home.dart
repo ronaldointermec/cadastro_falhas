429ef41431e228088010feecfe5891004763605c
Ronaldo Silva <ronaldo.intermec@gmail.com>
2024-02-09 09:29:12 -0300

    change title

--- lib/presentation/pages/home.dart
+++ lib/presentation/pages/home.dart
@@ -13,7 +13,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Registro de falhas'),
+        title: const Text('Registro de falhas by Honeywell'),
       ),
       body: Stack(
         alignment: Alignment.topCenter,
