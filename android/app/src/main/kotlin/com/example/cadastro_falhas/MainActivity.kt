d86d77314ca6b943b10d90f3104274e1870f8e42
Silva <ronaldo.silva2@honeywell.com>
2024-10-25 14:52:34 -0300

    clean code

--- android/app/src/main/kotlin/com/example/cadastro_falhas/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cadastro_falhas
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/infra/services/global.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-class Global{
-
-  static final FirebaseAuth auth = FirebaseAuth.instance;
-  static final usuarioAtual = auth.currentUser;
-}
